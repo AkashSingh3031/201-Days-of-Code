4c2e291cce28d885776d4fc2a1d69260fd40aad5
Akash Singh <akashsingh27101998@gmai.com>
2020-09-01 23:17:13 +0530

    HackerRank 30DaysOfCode (0-3)

---
+++ 013).  HackerRank [30DaysofCode]/0-3_Solution.py
@@ -0,0 +1,26 @@
+###### 1st Solution
+
+input_string = input()
+
+print("Hello, World."+'\n'+ input_string)
+
+
+
+###### 2nd Solution
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+a = int(input())
+b = float(input())
+c = input()
+
+print(i+a)
+print(d+b)
+print(s+c)
+
+
+##### 3rd Solution
+
+print(round(float(input())*(1+int(input())*.01+int(input())*.01)))
