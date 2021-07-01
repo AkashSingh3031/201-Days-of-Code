7ed5c8a1920d359810976f5eaa9e898380de43c3
Akash Singh <akashsingh27101998@gmai.com>
2021-07-01 16:05:08 +0530

    Add DSA 450 +LeetCode {6-10} Solution

---
+++ 186). LeetCode + 450_DSA Sheet Series [6-10]/_07)_Cyclic_Rotate_by_One.cpp
@@ -0,0 +1,36 @@
+//Initial Template for C++
+
+#include <stdio.h>
+
+void rotate(int arr[], int n);
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n] , i;
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        rotate(a, n);
+        for (i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+    }
+        return 0;
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+void rotate(int arr[], int n)
+{
+    int x = arr[n - 1], i;
+    for (i = n - 1; i > 0; i--)
+        arr[i] = arr[i - 1];
+    arr[0] = x;
+}
