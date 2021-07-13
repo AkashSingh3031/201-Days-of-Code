48a2e854dee725c3f7c580f2acff72f637d9a1a5
Akash Singh <akashsingh27101998@gmai.com>
2021-07-13 12:31:05 +0530

    Add LeetCode + 450 DSA Solution

---
+++ 193). LeetCode + 450_DSA Sheet Series [46-50  &  1-6]/_02)_Palindrome_String.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPlaindrome(string str)
+	{
+	    // Your code goes here
+	    int l = 0;
+        int h = str.length() - 1;
+      
+        while (h > l)
+        {
+            if (str[l++] != str[h--])
+            {
+                return 0;
+            }
+        }
+        return 1;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPlaindrome(s) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
