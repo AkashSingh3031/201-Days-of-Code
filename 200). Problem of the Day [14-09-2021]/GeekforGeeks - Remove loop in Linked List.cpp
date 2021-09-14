441dd838a126ae62550d38467004568910c2cab1
Akash Singh <60146338+AkashSingh3031@users.noreply.github.com>
2021-09-14 16:48:29 +0530

    Add GFG CPP Code

---
+++ 200). Problem of the Day [14-09-2021]/GeekforGeeks - Remove loop in Linked List.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+class Solution
+{
+    public:
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head)
+    {
+        // code here
+        // just remove the loop without losing any nodes
+        if(head==NULL ||head->next==NULL)
+            return;
+        
+        Node *s=head;
+        Node *f=head;
+        bool loop=false;
+        while(f && f->next && s)
+        {
+            f=f->next->next;
+            s=s->next;
+            if(f==s)
+            {
+                loop=true;
+                break;
+            }
+        }
+        if(!loop)
+            return;
+        
+        if(f==head)
+        {
+            while(f->next!=head)
+            {
+                f=f->next;
+            }
+            f->next=NULL;
+            return;
+        }
+        else
+        {
+            s=head;
+            while(f->next!=s->next)
+            {
+                f=f->next;
+                s=s->next;
+            }
+            f->next=NULL;
+            return;
+        }
+    }
+};
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        ob.removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
